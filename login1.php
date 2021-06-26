2add84ecf926bed89e3b562dd01d18b2abffe1bb
padmaja-gadiraju <78132653+padmaja-gadiraju@users.noreply.github.com>
2021-06-26 01:19:04 -0700

    Update login1.php

--- login1.php
+++ login1.php
@@ -17,7 +17,7 @@
      	<input type="password" name="password" placeholder="Password"><br>
 
      	<button type="submit">Login</button>
-          <a href="signup.php" class="ca">Create an account</a>
+          <a href="signup1.php" class="ca">Create an account</a>
      </form>
 </body>
 </html>
